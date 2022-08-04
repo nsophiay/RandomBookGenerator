ac19aea6d3fefb20ac8198644aa291f8819f5565
nsophiay <39935781+nsophiay@users.noreply.github.com>
2022-08-04 04:11:32 -0400

    Added more filter options

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ import { parse } from "./csv-parser/index.js";
 
 const reader = new FileReader();
 const TITLE=1, AUTHOR=2, RATING=8, NUMPAGES=11, YEARPUBLISHED=12, READ=14, DATEADDED=15, SHELVES=16;
-var numBooks, timePeriod, rating, shelves;
+var parsedCSV;
+var numBooks, timePeriod, rating;
+var numPagesMin, numPagesMax;
+var selectedShelves = [], selectedShelvesM = [], shelves = [];
+
+document.getElementById("csv").addEventListener("input", () => {
+            reader.addEventListener("load", processCSV, false);
+            reader.readAsText(document.getElementById("csv").files[0]);
+}, false);
 
 document.getElementById("submitButton").addEventListener("click", generate, false);
 
@@ -15,7 +23,15 @@ $(function(){
     });
 });
 
+function pickRandomNumber(lowerBound, upperBound){
+   return Math.floor((Math.random() * upperBound) + lowerBound)
+}
+
 function determineTimePeriod(str){
+
+    if(str == "Select a time period"){
+        str = "All time";
+    }
 
     var today = new Date();
     var year = today.getFullYear(), month = today.getMonth(), day = today.getDay();
@@ -62,6 +78,10 @@ function determineTimePeriod(str){
 
 function determineRating(str){
 
+    if(str == "Select a rating"){
+        str = "Any";
+    }
+
     var rating;
     switch(str){
 
@@ -70,9 +90,6 @@ function determineRating(str){
             break;
         case "4 stars+":
             rating = 4;
-            break;
-        case "5 stars":
-            rating = 5;
             break;
         case "Any":
             rating = 0;
@@ -95,11 +112,11 @@ function printBookInfo(array){
 function createList(array) {
 
     // Create the list element:
-    var list = document.createElement('ol');
+    let list = document.createElement('ol');
 
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         // Create the list item:
-        var item = document.createElement('li');
+        let item = document.createElement('li');
 
         // Set its contents:
         item.appendChild(document.createTextNode(printBookInfo(array[i])));
@@ -112,44 +129,189 @@ function createList(array) {
     return list;
 }
 
+function createCheckboxes(formGroupID, array, identifier) {
+
+    document.getElementById("message").remove();
+    var table = document.createElement('table');
+    var row;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if(i%2==0){
+            row = document.createElement('tr');
+        }
+
+        var cell = document.createElement('td');
+
+        // Create the input element
+        let input = document.createElement('input');
+        input.setAttribute('type', 'checkbox');
+        input.setAttribute('name', identifier);
+        input.setAttribute('value', array[i]);
+
+        // Create the label
+        let label = document.createElement('label');
+        label.setAttribute('for', array[i]);
+
+        // Set its contents
+        label.appendChild(document.createTextNode(array[i]));
+
+        // Add it to the form group
+        cell.appendChild(input);
+        cell.appendChild(label);
+        cell.appendChild(document.createElement('br'));
+
+        row.appendChild(cell);
+        if(i%2==0){
+            table.appendChild(row);
+        }
+    }
+
+    formGroupID.appendChild(table);
+
+}
+
+function createError(parentElement, message){
+
+    let errorDiv = document.createElement('div');
+    errorDiv.setAttribute("class", "alert alert-danger alert-dismissible fade show");
+    errorDiv.setAttribute("role", "alert");
+    errorDiv.innerHTML = message;
+
+    let closeButton = document.createElement('button');
+    closeButton.setAttribute("type", "button");
+    closeButton.setAttribute("class", "close");
+    closeButton.setAttribute("data-bs-dismiss", "alert");
+    closeButton.setAttribute("aria-label", "Close");
+
+    let spanElement = document.createElement('span');
+    spanElement.setAttribute("aria-hidden", "true");
+    spanElement.innerHTML = "&times;";
+
+    closeButton.appendChild(spanElement);
+    errorDiv.appendChild(closeButton);
+    parentElement.appendChild(errorDiv);
+
+}
+
 function processCSV(){
     
     // Parse CSV
     var booksCSV_string = reader.result;
-    var parsedCSV = parse(booksCSV_string);
+    parsedCSV = parse(booksCSV_string);
+
+    var encounteredShelf = new Map();
+    encounteredShelf.set("Bookshelves", true);
+    encounteredShelf.set("to-read", true);
+    encounteredShelf.set("currently-reading", true);
+    encounteredShelf.set("abandoned", true);
+    encounteredShelf.set("to-buy", true);
+    encounteredShelf.set("books-to-re-read", true);
+    encounteredShelf.set("have-re-read", true);
+
+    // Get list of distinct shelves
+    for(var i = 0; i < parsedCSV.length; i++){
+
+        let book = parsedCSV[i];
+        let bookShelves = book[SHELVES].split(", ");
+
+        // Loop through shelves the current book is in
+        // Add the shelf to the array if we haven't seen it before
+        for(var j = 0; j < bookShelves.length; j++){
+            let shelf = bookShelves[j];
+            if(!encounteredShelf.get(shelf) && shelf != ''){
+                shelves.push(shelf);
+                encounteredShelf.set(shelf, true);
+            }
+        }
+
+    }
+
+    console.log(shelves.toString());
+    createCheckboxes(document.getElementById("shelfChoicesMandatory"), shelves, 'shelfCheckboxesM');
+    createCheckboxes(document.getElementById("shelfChoices"), shelves, 'shelfCheckboxes');
+
+}
+
+function validateInputs(){
+
+    var errorMessage;
+    var noError = true;
+    
+    if(selectedShelvesM.some(elem => selectedShelves.includes(elem))){
+        errorMessage = "A shelf that you specified in the ALL form has also been specified in the ANY form. Please fix this and try again.";
+        noError = false;
+    }
+    else if(selectedShelvesM.includes("fiction") && selectedShelvesM.includes("non-fiction")){
+        errorMessage = "You have specified two mutually exclusive shelves in the ALL form. Please fix this and try again.";
+        noError = false;
+    }
+
+    if(!noError){
+        createError(document.getElementById("errorSpot"), errorMessage);
+    }
+    
+    return noError;
+
+}
+
+function randomBooks(){
 
     // Generate list of random books
     var bookArray = [];
     var encounteredNumbers = new Map();
+    let iteration = 0;
 
-    for(var i = 0; i < numBooks; i++){
+    while(bookArray.length < numBooks && iteration < parsedCSV.length){
 
-        // TODO: Add a check to see if there are enough books within the specified criteria
+        iteration++;
 
-        // Pick a random number within the range of books
-        var randomNumber = Math.floor((Math.random() * (parsedCSV.length-1)) + 1); 
+        // Pick a new random number within the range of books
+        let randomNumber = pickRandomNumber(1, parsedCSV.length-1);
+        if(encounteredNumbers.get(randomNumber)){
+            randomNumber = Math.floor((Math.random() * (parsedCSV.length-1)) + 1);
+        }
+        encounteredNumbers.set(randomNumber, true);
+
+
         var selectedBook = parsedCSV[randomNumber]; // Get random book
 
         // Check if book meets specified criteria
+
+        // Right date?
         var dateString = selectedBook[DATEADDED].split('/');
         var dateAdded = new Date(dateString[0], dateString[1], dateString[2]);
         var dateLimit = determineTimePeriod(timePeriod);
         const withinDateRange = dateAdded > dateLimit;
     
+        // Already read?
         const read = selectedBook[READ] !== "";
+
+        // Right rating?
         const withinRating = selectedBook[RATING] >= determineRating(rating);
 
-        const outsideOfCriteria = !withinDateRange || read || !withinRating;
+        // Right number of pages?
+        const withinPages = selectedBook[NUMPAGES] >= numPagesMin && selectedBook[NUMPAGES] <= numPagesMax;
 
-        // Make sure we didn't already use this random number
-        // and that the book is within the specified criteria
-        if(encounteredNumbers.get(randomNumber) || outsideOfCriteria){ 
-            i--;
-            continue;
-        } else {
-            bookArray.push(selectedBook); // Add to array of books
-            encounteredNumbers.set(randomNumber, true);
+        // Right shelves?
+        let bookShelves = selectedBook[SHELVES].split(", ");
+        var checkMandatoryShelves = true, checkShelves = true;
+
+        if(selectedShelvesM.length > 0){
+            checkMandatoryShelves = selectedShelvesM.every(elem => bookShelves.includes(elem));
         }
+        if(selectedShelves.length > 0){
+            checkShelves = selectedShelves.some(elem => bookShelves.includes(elem));
+        }
+        const withinShelves = checkMandatoryShelves && checkShelves;
+
+        const meetsCriteria = withinDateRange && !read && withinRating && withinPages && withinShelves;
+
+        // Check if the book is within the specified criteria
+        if(meetsCriteria){ 
+            bookArray.push(selectedBook);
+        }
+
     }
 
     console.log(bookArray);
@@ -157,29 +319,40 @@ function processCSV(){
     // Add list to HTML
     var listDiv = document.getElementById("bookList");
     listDiv.innerHTML = "";
-    listDiv.appendChild(createList(bookArray));
+
+    if(bookArray.length < numBooks){
+        listDiv.innerHTML = "No books match this criteria. Please loosen your criteria and try again.";
+    } else{
+        listDiv.appendChild(createList(bookArray));
+    }
 
 }
 
 function generate(){
-    
-    let booksCSV = document.getElementById("csv").files[0]; // Input file
 
     // Initialize criteria
     let criteriaForm = document.forms["criteria"];
     numBooks = criteriaForm["numBooks"].value; 
     timePeriod = document.getElementById("recency").innerHTML; 
     rating = document.getElementById("rating").innerHTML;
-    shelves = criteriaForm["fiction"].value;
+    numPagesMin = document.getElementById("slider-1").value;
+    numPagesMax = document.getElementById("slider-2").value;
 
-    // TODO: add function to validate inputs
+    let selectedShelvesMNodeList = document.querySelectorAll('input[name=shelfCheckboxesM]:checked');
+    selectedShelvesM = [];
+    for (const value of selectedShelvesMNodeList.values()) {
+        selectedShelvesM.push(value.getAttribute("value"));
+    }
 
-    console.log("You passed: " + numBooks + ", " + timePeriod + ", " + rating + ", " + booksCSV.name);
+    let selectedShelvesNodeList = document.querySelectorAll('input[name=shelfCheckboxes]:checked');
+    selectedShelves = [];
+    for (const value of selectedShelvesNodeList.values()) {
+        selectedShelves.push(value.getAttribute("value"));
+    }
 
-    // Parse the input file
-    if(booksCSV){
-        reader.addEventListener("load", processCSV, false);
-        reader.readAsText(booksCSV);
+    if(validateInputs()){
+        console.log("You passed: " + numBooks + ", " + timePeriod + ", " + rating + ", " + selectedShelves.toString() + ", num pages: " + numPagesMin + "-" + numPagesMax);
+        randomBooks();
     }
 
 }
